f311595325210bdb9ac58caa312333635d370647
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2021-09-04 10:37:18 +0900

    :+1: Fix pose_tracking_cpu_lib.h

--- mediapipe-custom/pose_tracking/desktop/pose_tracking_cpu_lib.h
+++ mediapipe-custom/pose_tracking/desktop/pose_tracking_cpu_lib.h
@@ -8,3 +8,4 @@ extern "C"
 {
 	CPPLIBRARY_API void init_pose_tracking(void);
 	CPPLIBRARY_API void process_pose_tracking(int width, int height, uint8* input_pixel_data, float* output_segmentation_mask, int64 frame_timestamp_us);
+}
