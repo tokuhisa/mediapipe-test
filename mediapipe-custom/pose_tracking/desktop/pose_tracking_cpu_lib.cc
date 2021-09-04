6914320f43ba2c89794757f46a6f02a9e1e24546
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2021-09-05 07:21:03 +0900

    :+1: Fix pose_tracking_cpu_lib

--- mediapipe-custom/pose_tracking/desktop/pose_tracking_cpu_lib.cc
+++ mediapipe-custom/pose_tracking/desktop/pose_tracking_cpu_lib.cc
@@ -1,12 +1,12 @@
 #include "pose_tracking_cpu_lib.h"
 
-#include "mediapipe/framework/calculator_framework.h"
-#include "mediapipe/framework/formats/image_frame.h"
-#include "mediapipe/framework/port/parse_text_proto.h"
-#include "mediapipe/framework/port/status.h"
+// #include "mediapipe/framework/calculator_framework.h"
+// #include "mediapipe/framework/formats/image_frame.h"
+// #include "mediapipe/framework/port/parse_text_proto.h"
+// #include "mediapipe/framework/port/status.h"
 
-constexpr char kInputStream[] = "image";
-constexpr char kOutputStreamSegmentationMask[] = "segmentation_mask";
+// constexpr char kInputStream[] = "image";
+// constexpr char kOutputStreamSegmentationMask[] = "segmentation_mask";
 
 float count = 0;
 CPPLIBRARY_API float count_up(void)
