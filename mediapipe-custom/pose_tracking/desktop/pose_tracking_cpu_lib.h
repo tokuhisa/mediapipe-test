2f5e313aab6f2f0a51b8ce6ab959c10e5c5fd0e4
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2021-09-04 09:35:27 +0900

    :+1: Add custom pose dll generator

---
+++ mediapipe-custom/pose_tracking/desktop/pose_tracking_cpu_lib.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define CPPLIBRARY_API __declspec(dllexport) 
+
+#include <cstdlib>
+
+extern "C"
+{
+	CPPLIBRARY_API void init_pose_tracking(void);
+	CPPLIBRARY_API void process_pose_tracking(int width, int height, uint8* input_pixel_data, float* output_segmentation_mask, int64 frame_timestamp_us);
