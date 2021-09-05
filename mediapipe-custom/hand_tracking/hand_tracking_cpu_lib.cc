f76ba5fb287e194e0ce14d573198552974a41c6a
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2021-09-05 18:16:25 +0900

    :+1: Fix custom hands

--- mediapipe-custom/hand_tracking/hand_tracking_cpu_lib.cc
+++ mediapipe-custom/hand_tracking/hand_tracking_cpu_lib.cc
@@ -18,5 +18,36 @@ CPPLIBRARY_API void test_uint8_array(uint8* data) {
 }
 
 CPPLIBRARY_API void test_float_array(float* data) {
-	
+
+}
+
+CPPLIBRARY_API void create_image_frame1(int width, int height, uint8* input_pixel_data)
+{
+	int number_Of_channels = 3; // SRGB format
+	int byte_depth = 1; // SRGB format
+	int width_step = width * number_Of_channels * byte_depth;
+    auto input_frame = absl::make_unique<mediapipe::ImageFrame>(mediapipe::ImageFormat::SRGB, width, height, width_step, input_pixel_data);
+}
+
+CPPLIBRARY_API void create_image_frame2(int width, int height, uint8* input_pixel_data)
+{
+	int number_Of_channels = 3; // SRGB format
+	int byte_depth = 1; // SRGB format
+	int width_step = width * number_Of_channels * byte_depth;
+    auto input_frame = mediapipe::ImageFrame(mediapipe::ImageFormat::SRGB, width, height, width_step, input_pixel_data);
+}
+
+
+CPPLIBRARY_API void create_image_frame3(int width, int height, uint8* input_pixel_data)
+{
+    auto input_frame = absl::make_unique<mediapipe::ImageFrame>(mediapipe::ImageFormat::SRGB, width, height, mediapipe::ImageFrame::kDefaultAlignmentBoundary);
+}
+
+CPPLIBRARY_API void create_image_frame4(int width, int height, uint8* input_pixel_data)
+{
+    auto input_frame = absl::make_unique<mediapipe::ImageFrame>(mediapipe::ImageFormat::SRGB, width, height, mediapipe::ImageFrame::kDefaultAlignmentBoundary);
+	int number_Of_channels = 3; // SRGB format
+	int byte_depth = 1; // SRGB format
+	int width_step = width * number_Of_channels * byte_depth;
+	input_frame->CopyPixelData(mediapipe::ImageFormat::SRGB, width, height, width_step, input_pixel_data, mediapipe::ImageFrame::kDefaultAlignmentBoundary); 
 }
--- mediapipe-custom/hand_tracking/hand_tracking_cpu_lib.h
+++ mediapipe-custom/hand_tracking/hand_tracking_cpu_lib.h
@@ -12,4 +12,8 @@ extern "C"
 	CPPLIBRARY_API void count_init(void);
 	CPPLIBRARY_API void test_uint8_array(uint8* data);
 	CPPLIBRARY_API void test_float_array(float* data);
+	CPPLIBRARY_API void create_image_frame1(int width, int height, uint8* input_pixel_data);
+	CPPLIBRARY_API void create_image_frame2(int width, int height, uint8* input_pixel_data);
+	CPPLIBRARY_API void create_image_frame3(int width, int height, uint8* input_pixel_data);
+	CPPLIBRARY_API void create_image_frame4(int width, int height, uint8* input_pixel_data);
 }
