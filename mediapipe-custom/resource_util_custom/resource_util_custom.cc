8d9ba19e7b013768fcfe85dceeb43832384464a9
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2021-09-11 00:34:27 +0900

    :+1: Add resource_util_custom

---
+++ mediapipe-custom/resource_util_custom/resource_util_custom.cc
@@ -0,0 +1,20 @@
+#include "resource_util_custom.h"
+
+CPPLIBRARY_API void set_custom_global_resource_provider(ResourceProvider* resource_provider) {
+  mediapipe::SetCustomGlobalResourceProvider([resource_provider](const std::string& path, std::string* output) -> ::absl::Status {
+    if (resource_provider(path.c_str(), output)) {
+      return absl::OkStatus();
+    }
+    return absl::FailedPreconditionError(absl::StrCat("Failed to read ", path));
+  });
+}
+
+
+CPPLIBRARY_API void set_custom_global_path_resolver(PathResolver* path_resolver) {
+  mediapipe::SetCustomGlobalPathResolver([path_resolver](const std::string& path) -> ::absl::StatusOr<std::string> {
+    auto resolved_path = path_resolver(path.c_str());
+
+    RET_CHECK_NE(resolved_path, nullptr);
+    return std::string(resolved_path);
+  });
+}
---
+++ mediapipe-custom/resource_util_custom/resource_util_custom.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define CPPLIBRARY_API __declspec(dllexport) 
+
+#include "mediapipe/util/resource_util_custom.h"
+
+
+extern "C"
+{
+	
+    typedef bool ResourceProvider(const char* path, std::string* output);
+    typedef const char* PathResolver(const char* path);
+
+    CPPLIBRARY_API void set_custom_global_resource_provider(ResourceProvider* resource_provider);
+    CPPLIBRARY_API void set_custom_global_path_resolver(PathResolver* path_resolver);
+}
