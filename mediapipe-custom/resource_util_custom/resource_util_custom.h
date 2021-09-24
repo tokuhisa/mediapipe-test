6ba13f5138b80cd51c33002918da908c64b1e237
Fumiaki TOKUHISA <tokuhisa.f@gmail.com>
2021-09-24 20:17:35 +0900

    :+1: Fix custom resource util

--- mediapipe-custom/resource_util_custom/resource_util_custom.h
+++ mediapipe-custom/resource_util_custom/resource_util_custom.h
@@ -13,8 +13,6 @@ extern "C"
 {
 	
     typedef bool ResourceProvider(const char* path, std::string* output);
-    typedef const char* PathResolver(const char* path);
 
     CPPLIBRARY_API void set_custom_global_resource_provider(ResourceProvider* resource_provider);
-    CPPLIBRARY_API void set_custom_global_path_resolver(PathResolver* path_resolver);
 }
